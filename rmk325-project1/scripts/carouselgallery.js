1505e2dbd0be8e6804afd71c85cf87aecda8f123
Rachel Korosi <rmk325@cornell.edu>
2022-11-28 01:16:43 -0500

    initial

---
+++ rmk325-project1/scripts/carouselgallery.js
@@ -0,0 +1,488 @@
+$("#dot1").addClass("dotsbg")
+$(".keys").addClass("hidden");
+$(".slope").addClass("hidden");
+$(".cornellstore").addClass("hidden");
+$(".axo").addClass("hidden");
+$(".slideshow").addClass("hidden");
+
+$("#wvbrbutton").click(function() {
+    $(".wvbr").removeClass("hidden");
+    $(".keys").addClass("hidden");
+    $(".slope").addClass("hidden");
+    $(".cornellstore").addClass("hidden");
+    $(".axo").addClass("hidden");
+
+    $("#dot1").addClass("dotsbg");
+    $("#dot2").removeClass("dotsbg");
+    $("#dot3").removeClass("dotsbg");
+    $("#dot4").removeClass("dotsbg");
+    $("#dot5").removeClass("dotsbg");
+
+    });
+
+$("#keysbutton").click(function() {
+    $(".wvbr").addClass("hidden");
+    $(".keys").removeClass("hidden");
+    $(".slope").addClass("hidden");
+    $(".cornellstore").addClass("hidden");
+    $(".axo").addClass("hidden");
+
+    $("#dot1").removeClass("dotsbg");
+    $("#dot2").addClass("dotsbg");
+    $("#dot3").removeClass("dotsbg");
+    $("#dot4").removeClass("dotsbg");
+    $("#dot5").removeClass("dotsbg");
+
+    });
+
+$("#slopebutton").click(function() {
+    $(".wvbr").addClass("hidden");
+    $(".keys").addClass("hidden");
+    $(".slope").removeClass("hidden");
+    $(".cornellstore").addClass("hidden");
+    $(".axo").addClass("hidden");
+
+    $("#dot1").removeClass("dotsbg");
+    $("#dot2").removeClass("dotsbg");
+    $("#dot3").addClass("dotsbg");
+    $("#dot4").removeClass("dotsbg");
+    $("#dot5").removeClass("dotsbg");
+
+    });
+
+$("#cornellstorebutton").click(function() {
+    $(".wvbr").addClass("hidden");
+    $(".keys").addClass("hidden");
+    $(".slope").addClass("hidden");
+    $(".cornellstore").removeClass("hidden");
+    $(".axo").addClass("hidden");
+
+    $("#dot1").removeClass("dotsbg");
+    $("#dot2").removeClass("dotsbg");
+    $("#dot3").removeClass("dotsbg");
+    $("#dot4").addClass("dotsbg");
+    $("#dot5").removeClass("dotsbg");
+
+    });
+
+$("#axobutton").click(function() {
+    $(".wvbr").addClass("hidden");
+    $(".keys").addClass("hidden");
+    $(".slope").addClass("hidden");
+    $(".cornellstore").addClass("hidden");
+    $(".axo").removeClass("hidden");
+
+    $("#dot1").removeClass("dotsbg");
+    $("#dot2").removeClass("dotsbg");
+    $("#dot3").removeClass("dotsbg");
+    $("#dot4").removeClass("dotsbg");
+    $("#dot5").addClass("dotsbg");
+    });
+
+$("#x-button").click(function() {
+  $(".slideshow").addClass("hidden");
+  $(".gallery").removeClass("hidden");
+  $(".footerp").removeClass("hidden");
+
+    });
+
+$("#1").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s1").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#2").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s2").removeClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#3").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s3").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#4").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s4").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#5").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s5").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#6").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s6").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#7").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s7").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#8").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s8").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#9").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s9").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#10").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s10").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#11").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s11").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#12").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s12").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#13").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s13").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#14").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s14").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#15").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s15").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s1").addClass("hidden");
+    $("#s16").addClass("hidden");
+    });
+
+$("#16").click(function() {
+    $(".gallery").addClass("hidden");
+    $(".footerp").addClass("hidden");
+    $(".slideshow").removeClass("hidden");
+    $("#s16").removeClass("hidden");
+    $("#s2").addClass("hidden");
+    $("#s3").addClass("hidden");
+    $("#s4").addClass("hidden");
+    $("#s5").addClass("hidden");
+    $("#s6").addClass("hidden");
+    $("#s7").addClass("hidden");
+    $("#s8").addClass("hidden");
+    $("#s9").addClass("hidden");
+    $("#s10").addClass("hidden");
+    $("#s11").addClass("hidden");
+    $("#s12").addClass("hidden");
+    $("#s13").addClass("hidden");
+    $("#s14").addClass("hidden");
+    $("#s15").addClass("hidden");
+    $("#s1").addClass("hidden");
+    });
+
+const SLIDES = $(".slides > * ");
+const DOTS = $(".dots > *")
+
+/* See "show next slide snippet" to use */
+function nextSlide() {
+  let nextNum = SLIDES.index($(".slides > *:not(.hidden)")) + 1 + 1; /* +1 for 0-index array */
+  if (nextNum > SLIDES.length) {
+    nextNum = 1;
+  }
+  showSlide(nextNum);
+}
+
+/* See "show previous slide snippet" to use */
+function prevSlide() {
+  let prevNum = SLIDES.index($(".slides > *:not(.hidden)")) - 1 + 1; /* +1 for 0-index array */
+  if (prevNum <= 0) {
+    prevNum = SLIDES.length;
+  }
+  showSlide(prevNum);
+}
+
+/* See "show slide X snippet" to use */
+function showSlide(num) {
+  let index = num - 1;
+  let currentSlide = SLIDES.eq(index);
+  let currentDot = DOTS.eq(index);
+  SLIDES.addClass("hidden");
+  currentSlide.removeClass("hidden");
+  DOTS.removeClass("dotsbg")
+  currentDot.addClass("dotsbg")
+}
+
+
+$("#next-button").click(function() {
+    nextSlide();
+  });
+
+$("#prev-button").click(function() {
+    prevSlide();
+  });
+
+$("#next-button2").click(function() {
+    nextSlide();
+  });
+
+$("#prev-button2").click(function() {
+    prevSlide();
+  });
